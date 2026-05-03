298492d39e16947a291c0eb7a8d6e1f4f9746d4a
Acer - Kemal <kemal.satya.wibowo1@gmail.com>
2026-05-03 12:54:53 +0700

    feat(openapi.yaml): implement scalar to simplify api visuals

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
+import fs from 'fs';
+import { apiReference } from '@scalar/express-api-reference';
 import appRoutes from './routes/index.js';
 import {
   startReminderCron,
@@ -11,6 +13,17 @@ const PORT = 5000;
 const app = express();
 
 app.use(express.json());
+
+// Scalar API Reference
+app.use(
+  '/reference',
+  apiReference({
+    theme: 'purple',
+    spec: {
+      content: fs.readFileSync('./openapi.yaml', 'utf-8'),
+    },
+  })
+);
 
 // Menghubungkan semua route dari folder routes
 app.use('/api', appRoutes);
